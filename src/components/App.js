d2870cf454699a9bed8c00701cb61d224231ab16
tunde <icepaigez@gmail.com>
2021-11-03 15:29:12 +0100

    feat:installed the identicon module

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,14 @@ class App extends Component {
     super()
     this.state = {
       web3: {},
-      connectedUser:""
+      connectedUser:"",
+      connected: false,
     }
   }
 
   loadWeb3 = async() => {
     if (window.ethereum) {
       let web3 = await new Web3(Web3.givenProvider || "http://localhost:7545");
-      //await window.ethereum.request({ method: 'eth_requestAccounts' });
       this.setState({ web3 });
       await this.loadBlockchainData(web3);
     } else {
@@ -26,21 +26,46 @@ class App extends Component {
   }
 
   loadBlockchainData = async (web3) => {
-    const accounts = await web3.eth.getAccounts();
-    this.setState({
-      connectedUser: accounts[0]
-    })
+    // const accounts = await web3.eth.getAccounts();
+    
+  }
+
+  connect = async () => {
+    try {
+      let acc = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      if (acc.length > 0) {
+        localStorage.setItem("connected", true);
+        this.setState({
+          connectedUser: acc[0],
+          connected: JSON.parse(localStorage.getItem("connected"))
+        })
+      }
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   async componentDidMount() {
     await this.loadWeb3()
   }
 
+  async componentDidUpdate(prevProps, prevState) {
+    const { web3 } = this.state;
+    let acc = await web3.eth.getAccounts();
+    let newConnection = JSON.parse(localStorage.getItem("connected"))
+    if (prevState && prevState.connected !== newConnection) {
+      this.setState({
+        connected: newConnection,
+        connectedUser: acc[0]
+      })
+    }
+  }
+
   render() {
-    const { connectedUser } = this.state;
+    const { connectedUser, connected } = this.state;
     return (
       <div className="app">
-        <Navbar user={connectedUser}/>
+        <Navbar user={connectedUser} connect={this.connect} connected={connected}/>
       </div>
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Identicon from "identicon.js"
 import "./navbar.css";
 
-const Navbar = ({ user }) => {
+const Navbar = ({ user, connect, connected }) => {
+	let data;
+	if (user) {
+		data = new Identicon(user, 420).toString();
+	}
 	return(
 		<div className="navbar">
 			<h1 className="logo">STCH</h1>
-			<div className="wallet">Connect Wallet</div>
+			{ connected ? <div className="user">
+							{ user }
+							<img width="30" height="30" alt="" src={`data:image/png;base64,${data}`}/>
+						  </div> 
+						: <div onClick={connect} className="wallet">Connect Wallet</div> }
 		</div>
 	)
 }
