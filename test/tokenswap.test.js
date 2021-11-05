8a09f7dd4fb2b33cc89b87dcfa110e9b0ade4da7
tunde <icepaigez@gmail.com>
2021-11-05 13:15:31 +0100

    testing the token exchange smart contracts

--- test/tokenswap.test.js
+++ test/tokenswap.test.js
@@ -1,1 +1,30 @@
 const TokenSwap = artifacts.require("TokenSwap");
+const STCHTOKEN = artifacts.require("STCHTOKEN");
+
+contract("TokenSwap", ([deployer, ...others]) => {
+
+	let instance;
+	let token;
+	beforeEach("should set up the contract instance", async() => {
+		instance = await TokenSwap.deployed();
+		token = await STCHTOKEN.deployed();
+	})
+
+	it("should accept Matic/Eth and sell Stch tokens to the buyer", async() => {
+		instance = await TokenSwap.deployed();
+		const buy_token = await instance.buyToken({from:deployer, value:web3.utils.toWei("0.04", "ether")});
+		const exchangeMaticBalance = await web3.eth.getBalance(instance.address);
+
+		let tokenEthPrice = await instance.tokenPriceInEth();
+		tokenEthPrice = web3.utils.toBN(tokenEthPrice)
+		let maticValue = await web3.utils.toWei("0.04", "ether");
+		maticValue = web3.utils.toBN(maticValue)
+		const tokenSold = maticValue/tokenEthPrice;
+		let exchangeTokenBalance = await token.balanceOf(instance.address);
+		exchangeTokenBalance = web3.utils.toBN(exchangeTokenBalance)
+		const exchTokenBalanceAfterSale = exchangeTokenBalance - tokenSold;
+
+		assert.equal(exchangeMaticBalance, web3.utils.toWei("0.04", "ether"));	
+		assert.equal(exchangeTokenBalance, exchTokenBalanceAfterSale);
+	})
+})
